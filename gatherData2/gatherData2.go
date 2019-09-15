03e9c303ed8ac59739fa299415b74f42615fec59
Dominik Rabij <dominik.rabij@gmail.com>
2019-09-15 20:37:58 +0200

    (gatherData2) added root.value, flags & increased capacities

--- gatherData2/gatherData2.go
+++ gatherData2/gatherData2.go
@@ -13,36 +13,53 @@ import (
 	"github.com/jamOne-/kiwi-zero/reversi"
 )
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
-	var simulationsFlag = flag.Int("simulations", 10000, "number of simulations")
+type config struct {
+	games           int
+	mctsSimulations int
+	mctsThreads     int
+}
+
+func get_config() *config {
+	var gamesFlag = flag.Int("games", 10000, "number of games to be simulated")
+	var mctsSimulationsFlag = flag.Int("mctsSimulations", 1000, "MCTS Simulations")
+	var mctsThreadsFlag = flag.Int("mctsThreads", 4, "MCTS Threads")
 	flag.Parse()
 
-	var NUMBER_OF_SIMULATIONS = *simulationsFlag
+	return &config{*gamesFlag, *mctsSimulationsFlag, *mctsThreadsFlag}
+}
 
-	averageTime := 0
+func main() {
+	config := get_config()
+	NUMBER_OF_GAMES := config.games
+	MCTS_SIMULATIONS := config.mctsSimulations
+	MCTS_THREADS := config.mctsThreads
 
 	file, _ := os.Create(strings.Replace(time.Now().String()[:19], ":", "", -1) + ".txt")
+	defer file.Close()
 
-	for i := 0; i < NUMBER_OF_SIMULATIONS; i++ {
-		debugStep := NUMBER_OF_SIMULATIONS / 10000
-		if debugStep == 0 || true {
+	rand.Seed(time.Now().UnixNano())
+	averageTime := 0
+
+	for i := 0; i < NUMBER_OF_GAMES; i++ {
+		debugStep := NUMBER_OF_GAMES / 1000
+		if debugStep == 0 {
 			debugStep = 1
 		}
 
 		if i%debugStep == 0 {
-			fmt.Printf("%v/%v (%.2f%%)\t%v left\n", i+1, NUMBER_OF_SIMULATIONS, float32(i+1)*100.0/float32(NUMBER_OF_SIMULATIONS), time.Duration(averageTime*(NUMBER_OF_SIMULATIONS-i))*time.Nanosecond)
+			fmt.Printf("%v/%v (%.2f%%)\t%v left\n", i+1, NUMBER_OF_GAMES, float32(i+1)*100.0/float32(NUMBER_OF_GAMES), time.Duration(averageTime*(NUMBER_OF_GAMES-i))*time.Nanosecond)
 		}
 
 		timeStart := time.Now()
 		game := reversi.NewGame()
-		player := mcts.NewThreadedMonteCarloTreeSearchPlayer(1000, 4)
+		player := mcts.NewThreadedMonteCarloTreeSearchPlayer(MCTS_SIMULATIONS, MCTS_THREADS)
 
-		boards := make([]string, 70)
-		probs := make([][]float64, 70)
-		turns := make([]int8, 70)
+		boards := make([]string, 100)
+		probs := make([][]float64, 100)
+		turns := make([]int8, 100)
+		values := make([]float64, 100)
 
-		finished, result := game.IsGameFinished()
+		finished, _ := game.IsGameFinished()
 		for !finished {
 			moveRoot := player.SelectMoveWithRoot(game)
 			move := moveRoot.Move
@@ -52,11 +69,16 @@ func main() {
 			board := game.SerializeBoard(game.Turn == reversi.WHITE)
 			turn := len(game.History)
 
+			if turn >= 100 {
+				fmt.Println(turn)
+			}
+
 			boards[turn] = board
 			probs[turn] = moveProbs
 			turns[turn] = game.Turn
+			values[turn] = float64(root.V) / float64(root.N)
 
-			finished, result = game.MakeMove(move)
+			finished, _ = game.MakeMove(move)
 		}
 
 		index := rand.Intn(len(game.History))
@@ -67,13 +89,12 @@ func main() {
 			fmt.Fprintf(file, "%.5f ", prob)
 		}
 
-		fmt.Fprintf(file, "%d\n", result*turns[index])
+		fmt.Fprintf(file, "%.5f ", values[index])
+		fmt.Fprintf(file, "%d\n", turns[index])
 
 		timeDuration := time.Since(timeStart)
 		averageTime += (int(timeDuration) - averageTime) / (i + 1)
 	}
-
-	file.Close()
 }
 
 func getMoveProbs(game *reversi.Game, root *mcts.Node) []float64 {
