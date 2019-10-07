ec93dabb1010836b035baa386d3a3bee2f0bde72
jamone- <dominik.rabij@gmail.com>
2019-10-07 19:31:10 +0200

    (experiment1) ReversiToFeatures refactor

--- experiment1/reversiValueFns.go
+++ experiment1/reversiValueFns.go
@@ -6,42 +6,48 @@ import (
 	"github.com/jamOne-/kiwi-zero/game"
 	"github.com/jamOne-/kiwi-zero/minMaxPlayer"
 	"github.com/jamOne-/kiwi-zero/reversi"
-	"github.com/jamOne-/kiwi-zero/utils"
 )
+
+var NUMBER_OF_FEATURES = 8*8 + 2 // fields + count + mobility
 
 func createWeightedReversiFn(weights *mat.VecDense) minMaxPlayer.ValueFn {
 	return func(g game.Game) float64 {
 		reversiGame := g.(*reversi.ReversiGame) // nieładnie, ale brak generyków to jest jakiś dramat
-		board := utils.Int8SliceToVecDense(reversiGame.Board)
-		totalScore := 0.0
 
-		countWeight := weights.AtVec(64)
-		countDifference := mat.Sum(board)
-		totalScore += countWeight * countDifference
-
-		weightedPositions := mat.Dot(board, weights.SliceVec(0, 64))
-		totalScore += weightedPositions
-
-		mobilityWeight := weights.AtVec(64)
-		currentPlayer := reversiGame.GetCurrentPlayerColor()
-		reversiGame.Turn = game.BLACK
-		blackMobility := len(reversiGame.GetPossibleMoves())
-		reversiGame.Turn = game.WHITE
-		whiteMobility := len(reversiGame.GetPossibleMoves())
-		reversiGame.Turn = currentPlayer
-		totalScore += mobilityWeight * float64(blackMobility-whiteMobility)
+		features := ReversiToFeatures(reversiGame)
+		totalScore := mat.Dot(features, weights)
 
 		return totalScore
 	}
 }
 
 func getInitialWeights() *mat.VecDense {
-	weightsLength := 8*8 + 2 // fields + countWeight + mobilityWeight
-	weights := make([]float64, weightsLength)
+	weights := make([]float64, NUMBER_OF_FEATURES)
 
-	for i := 0; i < weightsLength; i++ {
+	for i := 0; i < NUMBER_OF_FEATURES; i++ {
 		weights[i] = 1
 	}
 
-	return mat.NewVecDense(weightsLength, weights)
+	return mat.NewVecDense(NUMBER_OF_FEATURES, weights)
+}
+
+func ReversiToFeatures(reversiGame *reversi.ReversiGame) *mat.VecDense {
+	features := mat.NewVecDense(NUMBER_OF_FEATURES, nil)
+
+	for i, field := range reversiGame.Board {
+		features.SetVec(i, float64(field))
+	}
+
+	countDifference := mat.Sum(features)
+	features.SetVec(64, countDifference)
+
+	currentPlayer := reversiGame.GetCurrentPlayerColor()
+	reversiGame.Turn = game.BLACK
+	blackMobility := len(reversiGame.GetPossibleMoves())
+	reversiGame.Turn = game.WHITE
+	whiteMobility := len(reversiGame.GetPossibleMoves())
+	reversiGame.Turn = currentPlayer
+	features.SetVec(65, float64(blackMobility-whiteMobility))
+
+	return features
 }
