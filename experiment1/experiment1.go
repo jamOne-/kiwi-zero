72d650e76ae8a29b6820ec11fd351a4c00fd9dd7
jamone- <dominik.rabij@gmail.com>
2019-10-11 18:39:29 +0200

    (experiment1) triangle board weights

--- experiment1/experiment1.go
+++ experiment1/experiment1.go
@@ -18,10 +18,12 @@ import (
 	"github.com/jamOne-/kiwi-zero/runner"
 )
 
-var EVALUATOR_GAMES = 10
+var BREAK_AFTER_NO_CHANGES = 20
+var EPSILON = 0.1
+var EVALUATOR_GAMES = 15
 var GAMES_PER_ITERATION = 20
-var ITERATIONS = 100
-var MAX_HISTORY_LENGTH = 128
+var ITERATIONS = 2000
+var MAX_HISTORY_LENGTH = 30000
 var MINMAX_DEPTH = 4
 var TRAINING_SIZE = 256
 var COMPARE_WITH_OLD_MINMAX = true
@@ -29,24 +31,11 @@ var COMPARE_WITH_OLD_MINMAX = true
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	// newWeights := mat.NewVecDense(len(AFTER_1000_ITERS_WEIGHTS), AFTER_1000_ITERS_WEIGHTS)
-	// previousWeights := mat.NewVecDense(len(PREVIOUS_WEIGHTS), PREVIOUS_WEIGHTS)
-
-	// newVFn := createWeightedReversiFn(newWeights)
-	// newMMPlayer := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, newVFn)
-
-	// previousVFn := createWeightedReversiFn(previousWeights)
-	// previousMMPlayer := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, previousVFn)
-
-	// // randomPlayer := randomPlayer.NewRandomPlayer()
-
-	// newWins := evaluator.ComparePlayers(reversiGameFactory, newMMPlayer, previousMMPlayer, 50)
-	// fmt.Println(newWins)
-
 	// bestWeights := getInitialWeights()
-	bestWeights := mat.NewVecDense(66, PREVIOUS_WEIGHTS)
-	valueFn := createWeightedReversiFn(bestWeights)
+	bestWeights := getTriangleInitialWeights()
+	valueFn := createWeightedReversiFn(ReversiToFeaturesTriangle, bestWeights)
 	bestPlayer := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, valueFn)
+	selfPlayPlayer := minMaxPlayer.NewEpsilonGreedyMinMaxPlayer(MINMAX_DEPTH, EPSILON, valueFn)
 
 	historyPositions := make([]*mat.VecDense, 0)
 	historyYs := make([]float64, 0)
@@ -56,12 +45,17 @@ func main() {
 	// fmt.Printf("MinMax won %d / %d games versus MCTS\n", minMaxWins, 2*EVALUATOR_GAMES)
 	minMaxWins := 0
 
-	oldWeights := mat.NewVecDense(len(PREVIOUS_WEIGHTS), PREVIOUS_WEIGHTS)
-	oldValueFn := createWeightedReversiFn(oldWeights)
+	// oldWeights := mat.NewVecDense(len(OLD_MINMAX_WEIGHTS), OLD_MINMAX_WEIGHTS)
+	// oldValueFn := createWeightedReversiFn(ReversiToFeatures, oldWeights)
+	// oldMinMaxPlayer := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, oldValueFn)
+	oldWeights := LoadWeightsFromFile("./weights_2019-10-10 231145.txt")
+	oldValueFn := createWeightedReversiFn(ReversiToFeaturesTriangle, oldWeights)
 	oldMinMaxPlayer := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, oldValueFn)
 
-	for iteration := 0; iteration < ITERATIONS; iteration++ {
-		fmt.Printf("Iteration %d / %d...\n", iteration+1, ITERATIONS)
+	lastIterationChange := -1
+
+	for iteration := 0; iteration < ITERATIONS && iteration-lastIterationChange < BREAK_AFTER_NO_CHANGES; iteration++ {
+		fmt.Printf("Iteration %d / %d (lastIterationChange=%d)...\n", iteration+1, ITERATIONS, lastIterationChange+1)
 
 		if COMPARE_WITH_OLD_MINMAX && (iteration+1)%10 == 0 {
 			numberOfGames := 20
@@ -69,10 +63,11 @@ func main() {
 			fmt.Printf("Current best player won %d / %d games versus OLD MinMax\n", bestWins, numberOfGames)
 		}
 
-		results, _ := runner.PlayNGames(reversiGameFactory, bestPlayer, bestPlayer, GAMES_PER_ITERATION)
+		results, totalPositions := runner.PlayNGames(reversiGameFactory, selfPlayPlayer, selfPlayPlayer, GAMES_PER_ITERATION)
 		// utils.SaveGameResultsToFile(results, strings.Replace(time.Now().String()[:19], ":", "", -1)+".txt")
 
-		Xs, ys := gameResultsToXsAndys2(results, 2)
+		Xs, ys := gameResultsToXsAndys(ReversiToFeaturesTriangle, results, totalPositions)
+		// Xs, ys := gameResultsToXsAndys2(ReversiToFeaturesTriangle, results, 10)
 		historyPositions = append(historyPositions, Xs...)
 		historyYs = append(historyYs, ys...)
 
@@ -87,20 +82,22 @@ func main() {
 		sgdResult := sgd.SGD(sgd.MeanSquaredErrorWithGradient, bestWeights, Xs, ys, map[string]float64{
 			"alpha0":     5e-5,
 			"alphaConst": 0,
-			"momentum":   0,
+			"momentum":   0.9,
 			"batch_size": 16,
 			"max_epochs": 10000,
 			"debug":      1})
 
-		newValueFn := createWeightedReversiFn(sgdResult.BestWeights)
+		newValueFn := createWeightedReversiFn(ReversiToFeaturesTriangle, sgdResult.BestWeights)
 		candidate := minMaxPlayer.NewMinMaxPlayer(MINMAX_DEPTH, newValueFn)
 		candidateWins := evaluator.ComparePlayers(reversiGameFactory, candidate, bestPlayer, EVALUATOR_GAMES)
 
 		fmt.Printf("New candidate won %d / %d games\n", candidateWins, EVALUATOR_GAMES)
 
-		if float64(candidateWins)/float64(EVALUATOR_GAMES) >= 0.5 {
+		if float64(candidateWins)/float64(EVALUATOR_GAMES) > 0.5 {
 			bestPlayer = candidate
 			bestWeights = sgdResult.BestWeights
+			selfPlayPlayer = minMaxPlayer.NewEpsilonGreedyMinMaxPlayer(MINMAX_DEPTH, EPSILON, newValueFn)
+			lastIterationChange = iteration
 		}
 
 		fmt.Println("")
@@ -119,7 +116,7 @@ func reversiGameFactory() game.Game {
 	return reversi.NewReversiGame()
 }
 
-func gameResultsToXsAndys(results []*runner.GameResult, totalPositions int) ([]*mat.VecDense, []float64) {
+func gameResultsToXsAndys(reversiToFeaturesFn ReversiToFeaturesFn, results []*runner.GameResult, totalPositions int) ([]*mat.VecDense, []float64) {
 	Xs := make([]*mat.VecDense, totalPositions)
 	ys := make([]float64, totalPositions)
 
@@ -127,7 +124,7 @@ func gameResultsToXsAndys(results []*runner.GameResult, totalPositions int) ([]*
 	for _, gameResult := range results {
 		for _, position := range gameResult.History {
 			reversiGame := position.(*reversi.ReversiGame)
-			Xs[positionIndex] = ReversiToFeatures(reversiGame)
+			Xs[positionIndex] = reversiToFeaturesFn(reversiGame)
 			ys[positionIndex] = float64(gameResult.Winner) * 100.0
 			positionIndex += 1
 		}
@@ -136,7 +133,7 @@ func gameResultsToXsAndys(results []*runner.GameResult, totalPositions int) ([]*
 	return Xs, ys
 }
 
-func gameResultsToXsAndys2(results []*runner.GameResult, positionsPerGame int) ([]*mat.VecDense, []float64) {
+func gameResultsToXsAndys2(reversiToFeaturesFn ReversiToFeaturesFn, results []*runner.GameResult, positionsPerGame int) ([]*mat.VecDense, []float64) {
 	totalPositions := len(results) * positionsPerGame
 	Xs := make([]*mat.VecDense, totalPositions)
 	ys := make([]float64, totalPositions)
@@ -146,7 +143,7 @@ func gameResultsToXsAndys2(results []*runner.GameResult, positionsPerGame int) (
 			positionIndex := resultIndex*positionsPerGame + i
 			position := gameResult.History[rand.Intn(len(gameResult.History))]
 			reversiGame := position.(*reversi.ReversiGame)
-			Xs[positionIndex] = ReversiToFeatures(reversiGame)
+			Xs[positionIndex] = reversiToFeaturesFn(reversiGame)
 			ys[positionIndex] = float64(gameResult.Winner) * 100.0
 		}
 	}
--- experiment1/reversiValueFns.go
+++ experiment1/reversiValueFns.go
@@ -12,13 +12,15 @@ import (
 	"github.com/jamOne-/kiwi-zero/reversi"
 )
 
+type ReversiToFeaturesFn func(reversiGame *reversi.ReversiGame) *mat.VecDense
+
 var NUMBER_OF_FEATURES = 8*8 + 2 // fields + count + mobility
 
-func createWeightedReversiFn(weights *mat.VecDense) minMaxPlayer.ValueFn {
+func createWeightedReversiFn(reversiToFeaturesFn ReversiToFeaturesFn, weights *mat.VecDense) minMaxPlayer.ValueFn {
 	return func(g game.Game) float64 {
 		reversiGame := g.(*reversi.ReversiGame) // nieładnie, ale brak generyków to jest jakiś dramat
 
-		features := ReversiToFeatures(reversiGame)
+		features := reversiToFeaturesFn(reversiGame)
 		totalScore := mat.Dot(features, weights)
 
 		return totalScore
@@ -55,7 +57,7 @@ func ReversiToFeatures(reversiGame *reversi.ReversiGame) *mat.VecDense {
 	// }
 
 	countFeature := mat.Sum(features)
-	features.SetVec(64, countFeature)
+	features.SetVec(NUMBER_OF_FEATURES-2, countFeature)
 
 	currentPlayer := reversiGame.GetCurrentPlayerColor()
 	reversiGame.Turn = game.BLACK
@@ -70,9 +72,52 @@ func ReversiToFeatures(reversiGame *reversi.ReversiGame) *mat.VecDense {
 	// 	mobilityFeature = -float64(whiteMobility) / float64(blackMobility+whiteMobility)
 	// }
 
-	features.SetVec(65, mobilityFeature)
+	features.SetVec(NUMBER_OF_FEATURES-1, mobilityFeature)
 
 	return features
+}
+
+func ReversiToFeaturesTriangle(reversiGame *reversi.ReversiGame) *mat.VecDense {
+	triangleNumberOfFeatures := 4 + 3 + 2 + 1 + 2
+	features := mat.NewVecDense(triangleNumberOfFeatures, nil)
+	board := reversiGame.Board
+
+	features.SetVec(0, float64(board[0]+board[7]+board[56]+board[63]))
+	features.SetVec(1, float64(board[1]+board[6]+board[8]+board[15]+board[48]+board[57]+board[55]+board[62]))
+	features.SetVec(2, float64(board[2]+board[5]+board[16]+board[23]+board[40]+board[47]+board[58]+board[61]))
+	features.SetVec(3, float64(board[3]+board[4]+board[24]+board[32]+board[31]+board[39]+board[59]+board[60]))
+	features.SetVec(4, float64(board[9]+board[14]+board[49]+board[54]))
+	features.SetVec(5, float64(board[10]+board[13]+board[17]+board[22]+board[41]+board[46]+board[50]+board[53]))
+	features.SetVec(6, float64(board[11]+board[12]+board[25]+board[33]+board[30]+board[38]+board[51]+board[52]))
+	features.SetVec(7, float64(board[18]+board[21]+board[45]+board[42]))
+	features.SetVec(8, float64(board[19]+board[20]+board[26]+board[29]+board[34]+board[37]+board[43]+board[44]))
+	features.SetVec(9, float64(board[27]+board[28]+board[35]+board[36]))
+
+	countFeature := mat.Sum(features)
+	features.SetVec(triangleNumberOfFeatures-2, countFeature)
+
+	currentPlayer := reversiGame.GetCurrentPlayerColor()
+	reversiGame.Turn = game.BLACK
+	blackMobility := len(reversiGame.GetPossibleMoves())
+	reversiGame.Turn = game.WHITE
+	whiteMobility := len(reversiGame.GetPossibleMoves())
+	reversiGame.Turn = currentPlayer
+
+	mobilityFeature := float64(blackMobility - whiteMobility)
+	features.SetVec(triangleNumberOfFeatures-1, mobilityFeature)
+
+	return features
+}
+
+func getTriangleInitialWeights() *mat.VecDense {
+	triangleNumberOfFeatures := 4 + 3 + 2 + 1 + 2
+	weights := make([]float64, triangleNumberOfFeatures)
+
+	for i := 0; i < triangleNumberOfFeatures; i++ {
+		weights[i] = 1
+	}
+
+	return mat.NewVecDense(triangleNumberOfFeatures, weights)
 }
 
 func SaveWeightsToFile(weights *mat.VecDense, fileName string) {
@@ -105,7 +150,7 @@ func LoadWeightsFromFile(fileName string) *mat.VecDense {
 	return mat.NewVecDense(len(weights), weights)
 }
 
-var PREVIOUS_WEIGHTS = []float64{20, -3, 11, 8, 8, 11, -3, 20,
+var OLD_MINMAX_WEIGHTS = []float64{20, -3, 11, 8, 8, 11, -3, 20,
 	-3, -7, -4, 1, 1, -4, -7, -3,
 	11, -4, 2, 2, 2, 2, -4, 11,
 	8, 1, 2, -3, -3, 2, 1, 8,
