21ed9a6b80b7631db102fc8c9cff8a030af51ec2
jamone- <dominik.rabij@gmail.com>
2021-02-16 21:39:32 +0100

    .

--- python/optimizer/Model.py
+++ python/optimizer/Model.py
@@ -1,74 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models, utils, regularizers
 
-
-def add_ResidualLayer(inputs, filters):
-    x = layers.Conv2D(
-        filters,
-        kernel_size=(3, 3),
-        padding='same',
-        # use_bias=False
-    )(inputs)
-    # x = layers.BatchNormalization()(x)
-    # x = layers.Activation('relu')(x)
-
-    x = layers.Conv2D(
-        filters,
-        kernel_size=(3, 3),
-        padding='same',
-        # use_bias=False
-    )(x)
-    # x = layers.BatchNormalization()(x)
-
-    # x = layers.add([x, inputs])
-    x = layers.Activation('relu')(x)
-
-    return x
-
-
-def add_ConvLayer(inputs, filters):
-    x = layers.Conv2D(
-        filters,
-        kernel_size=(3, 3),
-        padding='same',
-        activation='relu'
-    )(inputs)
-
-
-def add_ValueHead(model, filters):
-    model = layers.Conv2D(
-        1,
-        kernel_size=(1, 1),
-        padding='same',
-        # use_bias=False
-    )(model)
-    # model = layers.BatchNormalization()(model)
-    # model = layers.Activation('relu')(model)
-    model = layers.Flatten()(model)
-    model = layers.Dense(filters, activation='relu')(model)
-    model = layers.Flatten()(model)
-    model = layers.Dense(1)(model)
-    model = layers.Activation('tanh', name='value_out')(model)
-
-    return model
-
-
-def add_PolicyHead(model):
-    model = layers.Conv2D(
-        2,
-        kernel_size=(1, 1),
-        padding='same',
-        # use_bias=False
-    )(model)
-    model = layers.BatchNormalization()(model)
-    model = layers.Activation('relu')(model)
-    model = layers.Flatten()(model)
-    # TODO: parametrize number of possibilities
-    model = layers.Dense(8 * 8 + 1, name='policy_out')(model)
-
-    return model
-
-
 def get_model(
     input_shape=(8, 8, 3),
     policy_length=65,
@@ -97,7 +29,6 @@ def get_model(
         kernel_regularizer=regularizers.l2(regularizer_const),
         bias_regularizer=regularizers.l2(regularizer_const),
     )(value_out)
-    # value_out = layers.Dropout(0.25)(value_out)
     value_out = layers.Dense(1, activation='sigmoid', name='value_out')(value_out)
 
     policy_out = layers.Flatten()(model)
@@ -108,7 +39,6 @@ def get_model(
             kernel_regularizer=regularizers.l2(regularizer_const),
             bias_regularizer=regularizers.l2(regularizer_const),
         )(policy_out)
-        # policy_out = layers.Dropout(0.25)(policy_out)
     policy_out = layers.Dense(policy_length, activation='softmax', name='policy_out')(policy_out)
     
     ret = models.Model(
